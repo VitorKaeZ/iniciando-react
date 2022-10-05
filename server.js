8b6957aa6bb01bfb9d3a0bf6a060209b804dcc6e
VitorKaeZ <vitorcaetanoz@outlook.com>
2022-10-05 06:59:33 -0300

    .

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express ()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-app.use('/', express.static(__dirname, './dist'))
+app.use('/', express.static('./dist'))
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname,'dist','index.html'), function(err) {
