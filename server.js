0321e9846e820c2bdbe5ff3f05bba4018afac0c8
VitorKaeZ <vitorcaetanoz@outlook.com>
2022-10-05 06:56:28 -0300

    add:deploy

--- server.js
+++ server.js
@@ -16,4 +16,24 @@
 //     console.log(`JSON server is running! port:${port}`)
 // })
 
-console.log('batata')
+import express from 'express'
+import path from 'path'
+import {fileURLToPath} from 'url';
+
+const app = express ()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use('/', express.static(__dirname, './dist'))
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname,'dist','index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
+
+app.listen (process.env.PORT || 5000, console.log('tudo certo'))
